50fa0ae3644651910bbc28722fb3097c70692bca
Anca Prodan <79717589+Anca212000@users.noreply.github.com>
2025-07-09 11:29:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-   </HashRouter>
-  </React.StrictMode>,
-);
+ReactDOM.render(<App />, document.getElementById('root'));
